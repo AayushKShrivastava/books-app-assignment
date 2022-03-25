2768ec9e2a66d5f23064ab989a38b645cd492bc6
Aayush Shrivastava <2019002@iiitdmj.ac.in>
2022-03-25 18:12:08 +0530

    updated package.json

--- src/layout/Customization/index.js
+++ src/layout/Customization/index.js
@@ -26,7 +26,7 @@ import SubCard from 'ui-component/cards/SubCard';
 import AnimateButton from 'ui-component/extended/AnimateButton';
 import { SET_BORDER_RADIUS, SET_FONT_FAMILY } from 'store/actions';
 import { gridSpacing } from 'store/constant';
-// concat 'px'
+
 function valueText(value) {
     return `${value}px`;
 }
